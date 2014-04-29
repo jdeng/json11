3d94826d1d76c7adc860cb76cafae9da516bc8c9
Jack <jackdeng@gmail.com>
2014-04-29 12:26:03 +0800

    t

--- bench.cpp
+++ bench.cpp
@@ -1,19 +1,23 @@
+// g++ -std=c++11 -Ofast -o bench bench.cpp
+
 #include "json11.h"
 #include "json11.cpp"
 #include "iostream"
 
+namespace json = json11;
 int main(int, char *[])
 {
-	const size_t N = 100000;
-	std::vector<json11::Value> vals;
+	const size_t N = 1000000;
+	std::vector<json::Value> vals;
 	for (int i=0; i<N; ++i) {
-		vals.push_back(json11::Object{{"id", i}, {"value", std::to_string(i)}});
+		vals.push_back(json::Object{{"id", i}, {"value", std::to_string(i)}});
 	}
 
+	json::Value arr(std::move(vals));
 	size_t sum = 0;
 	for (int i=0; i<N; ++i) {
-		sum += vals[i].to_string().size();
-		sum += vals[i]["value"].string_value().size();
+		sum += arr[i].to_string().size();
+		sum += arr[i]["value"].string_value().size();
 	}
 
 	std::cout << "total: " << sum << std::endl;
